c6bc2392fd1231e31de64e7a6761764642b0ffd0
Will Rossiter <will@fullscreen.io>
2022-10-10 12:22:45 +1300

    fix: cors access to actions when used on non-localhost domains

--- src/Controllers/StorybookController.php
+++ src/Controllers/StorybookController.php
@@ -2,35 +2,70 @@
 
 namespace Wilr\SilverStripe\Storybook\Controllers;
 
-use Wilr\SilverStripe\Storybook;
+use Wilr\SilverStripe\Storybook\Storybook;
 use SilverStripe\Core\ClassInfo;
 use SilverStripe\Control\Controller;
 
 class StorybookController extends Controller
 {
-    private static $register = [];
+    private static $url_handlers = [
+        '$ID' => 'index',
+    ];
+
+    public function init()
+    {
+        parent::init();
+
+        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            $response = $this->getResponse()
+                ->addHeader('Access-Control-Allow-Origin', '*');
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+                $response = $response->addHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            }
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+                $response = $response->addHeader(
+                    'Access-Control-Allow-Headers',
+                    $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']
+                );
+            }
+
+            $response->output();
+            exit;
+        }
+    }
+
+    public function allowedActions($limitToClass = null)
+    {
+        return true;
+    }
+
 
     public function index()
     {
-        $key = $this->request->param('id');
+        $response = $this->getResponse()
+            ->addHeader('Access-Control-Allow-Origin', '*');
 
+        $key = $this->request->param('ID');
         if (!$key) {
-            return $this->httpError(400);
+            return $response->setStatusCode(404);
         }
 
         $sources = ClassInfo::implementorsOf(Storybook::class);
         $stories = [];
 
         foreach ($sources as $source) {
-            $stories = array_merge($stories, $source->getStories());
+            $stories = array_merge($stories, singleton($source)->getStories());
         }
 
         if (!isset($stories[$key])) {
-            return $this->httpError(404);
+            return $response->setStatusCode(404);
         }
 
-        $template = $stores[$key]();
+        $template = $stories[$key]();
+        $response->setBody($template);
 
-        return $template;
+        return $response;
     }
 }
